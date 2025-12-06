904258540a279103da81860a0f6e09cd94342331
Yuya Nishihara <yuya@tcha.org>
2025-12-06 13:51:07 +0900

    cli: bookmark: add --remote argument to track/untrack, deprecate name@remote
    These two are the last commands which don't support logical operators in string
    patterns. The old <kind>:<name>@<remote> syntax had various problems including:
    
     1. substring patterns look weird (e.g. `substring:x@y` means `*x*@*y*`)
     2. cannot express "all but <name> for all remotes" (e.g. `(~gh-pages)@(*)`)
    
    In addition to that, the revset parser doesn't support `<name>@<remote>`
    prefixed by `<kind>:`.
    
    This patch introduces separate --remote argument to address these problems. The
    default is `glob:*` (or `~git`), so we wouldn't have to specify the remote in
    many cases. One caveat is that `jj bookmark track` is not idempotent if there
    are multiple remotes:
    
        # there are two remotes: origin and upstream,
        # and only foo@origin exists
        $ jj bookmark track foo
        # tracks foo@origin and creates new local bookmark foo
        $ jj bookmark track foo
        # tracks absent foo@upstream as we now have a local bookmark
    
    This is wild. We might want to add a flag or a new command to track absent
    remote bookmarks to push.
    
    "Unmatched names" warnings are now emitted for bookmark and remote names
    separately. To keep the implementation simple, the search space isn't restricted
    by the other parameter. For example, "jj bookmark track foo --remote=bar" won't
    show a warning if "foo" exists locally or in any remote.
    
    Closes #4260

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -3110,8 +3110,9 @@ impl DiffSelector {
     }
 }
 
+// TODO: Delete in jj 0.43+
 #[derive(Clone, Debug)]
-pub struct RemoteBookmarkNamePattern {
+pub(crate) struct RemoteBookmarkNamePattern {
     pub bookmark: StringPattern,
     pub remote: StringPattern,
 }
